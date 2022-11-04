214e7e7722062d54a0c11dad53f0933735680442
shamitomita <shamitomita@gmail.com>
2022-11-04 18:45:21 -0400

    commit

--- Udacity/First Go Round.swift
+++ Udacity/First Go Round.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+var question = "Ready to write your first lines of Swift?"
+var response = "Heck yeah, I'm ready!"
+var salutation = "Hello, world!!!"
+
+// Example 1
+let encouragement = "You can do it!"
+var customizedEncouragement = "You can do it, Stephanie!"
+customizedEncouragement = "You can do it, Ryder!"
+
+// Example 2
+let birthYear = 2008
+var currentYear = 2016
+var age = currentYear - birthYear
+currentYear = 2017
+age = currentYear - birthYear
