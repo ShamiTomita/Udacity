1a821f3c63b002c057fbc794b2a4b5eee968d39b
shamitomita <shamitomita@gmail.com>
2022-11-04 18:26:58 -0400

    Initial Commit

---
+++ Udacity/UdacityApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdacityApp.swift
+//  Udacity
+//
+//  Created by Shami Tomita on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct UdacityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
