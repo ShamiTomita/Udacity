3cda228b517fdac94158f8e30dbfac0243d532c1
shamitomita <shamitomita@gmail.com>
2022-11-11 18:04:42 -0500

    switch case

--- Udacity/SwfitBook.playground/Contents.swift
+++ Udacity/SwfitBook.playground/Contents.swift
@@ -15,3 +15,30 @@ if let name = optionalName{
 }
 
 print(greeting) //prints "fine then
+
+
+let nickname: String? = nil
+let fullName: String = "John Appleseed"
+let informalGreeting = "Hi \(nickname ?? fullName)"
+
+print(informalGreeting) //"Hi John AppleSeed"
+
+if let nickname {
+    print("Hey, \(nickname)")
+}
+
+//
+
+var vegetable = "red pepper"
+vegetable = "celery"
+switch vegetable {
+case "celery":
+    print("Add some raisins and make ants on a log.")
+case "cucumber", "watercress":
+    print("That would make a good tea sandwich.")
+case let x where x.hasSuffix("pepper"):
+    print("Is it a spicy \(x)?")
+default:
+    print("Everything tastes good in soup.")
+}
+// Prints "Is it a spicy red pepper?”
