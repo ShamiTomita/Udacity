da5ade52493308c19749e0953abf12d13d246564
shamitomita <shamitomita@gmail.com>
2022-11-11 17:57:57 -0500

    fine then!

--- Udacity/SwfitBook.playground/Contents.swift
+++ Udacity/SwfitBook.playground/Contents.swift
@@ -5,11 +5,13 @@ var optionalString: String? = "Hello"
 print(optionalString == nil)
 //Prints False
 
-var optionalName: String? = "John"
+var optionalName: String? = nil
 var greeting = "Hello!"
 
 if let name = optionalName{
     greeting = "Hello, \(name)"
+} else {
+    greeting = "Fine Then!"
 }
 
-print(greeting)
+print(greeting) //prints "fine then
