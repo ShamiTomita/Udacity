073ef77be0604fb8b4f044e760c3de8925d1b748
shamitomita <shamitomita@gmail.com>
2022-11-12 15:12:14 -0500

    for loop

--- Udacity/SwfitBook.playground/Contents.swift
+++ Udacity/SwfitBook.playground/Contents.swift
@@ -42,3 +42,21 @@ default:
     print("Everything tastes good in soup.")
 }
 // Prints "Is it a spicy red pepper?”
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13],
+    "Fibonacci": [1, 1, 2, 3, 5, 8],
+    "Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+var varb = 0
+for (_, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+print(largest)
+// Prints "25”
+
