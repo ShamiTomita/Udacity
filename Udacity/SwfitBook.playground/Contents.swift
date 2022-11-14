bb23e9d5bbd6d6d4d77dc73f26bb68be78923834
shamitomita <shamitomita@gmail.com>
2022-11-13 21:46:09 -0500

    functions and closures cont

--- Udacity/SwfitBook.playground/Contents.swift
+++ Udacity/SwfitBook.playground/Contents.swift
@@ -94,3 +94,27 @@ func greet(_ person: String, on day: String) -> String {
 }
 greet("John", on: "Wednesday")
 
+//tuples???
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+// Prints "120"
+print(statistics.2)
+// Prints "120”
+
