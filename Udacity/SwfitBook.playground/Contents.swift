5b71029b02c7665647d72850d243fbccc41ae471
shamitomita <shamitomita@gmail.com>
2022-11-13 21:48:49 -0500

    functions can take functions as an argument

--- Udacity/SwfitBook.playground/Contents.swift
+++ Udacity/SwfitBook.playground/Contents.swift
@@ -139,3 +139,18 @@ func makeIncrementer() -> ((Int) -> Int) {
 }
 var increment = makeIncrementer()
 increment(7)
+
+//functions can take another function as one of its arguments
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
